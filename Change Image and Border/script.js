3b8da0bfbb4e07f9cde64f1adb95d9992ad8727f
Nou Ratanak Panhasak <ratanaknou126629@gmail.com>
2026-01-23 11:58:51 +0700

    Extra Feature for changing image and border

--- Change Image and Border/script.js
+++ Change Image and Border/script.js
@@ -1,9 +1,38 @@
+const colorMap = {
+  'red': [255, 0, 0],
+  'blue': [0, 0, 255],
+  'green': [0, 255, 0],
+  'orange': [255, 165, 0]
+};
+
+let borderColor = '#ccc';
+let borderOpacity = 1.0;
+
 function change_image(images_id, images_name) {
   document.getElementById(images_id).src =
     "images/" + images_name + ".jpg";
 }
 
 function change_border(images_id, color) {
-  document.getElementById(images_id).style.borderColor = color;
-  document.getElementById(images_id).style.borderWidth = '10px';
+  borderColor = color;
+  updateBorder(images_id);
+  document.querySelector('h1').style.color = color;
+}
+
+function change_opacity(images_id, opacity) {
+  borderOpacity = opacity;
+  updateBorder(images_id);
+}
+
+function updateBorder(images_id) {
+  const img = document.getElementById(images_id);
+  let rgb;
+  if (borderColor in colorMap) {
+    rgb = colorMap[borderColor];
+  } else if (borderColor === '#ccc') {
+    rgb = [204, 204, 204];
+  } else {
+    rgb = [204, 204, 204];
+  }
+  img.style.border = `10px solid rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${borderOpacity})`;
 }
